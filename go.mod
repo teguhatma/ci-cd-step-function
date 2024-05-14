2aa4be63055be2626cfa4c043a40eee22fe690fa
teguhatma <teguh.yudha@finaccel.co>
2024-05-15 01:33:03 +0700

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/teguhatma/ci-cd-step-function
+
+go 1.22.2
+
+require github.com/gorilla/mux v1.8.1 // indirect
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/api/health", func(w http.ResponseWriter, r *http.Request) {
+		// an example API handler
+		json.NewEncoder(w).Encode(map[string]bool{"ok": true})
+	})
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    "127.0.0.1:8000",
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	fmt.Println("server started")
+
+	if err := srv.ListenAndServe(); err != nil {
+		fmt.Println("server stopped")
+		log.Fatal(err)
+	}
+}
